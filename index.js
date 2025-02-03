0fda0e257e258b11fbb217de32b42abb7a3ce37f
Imran Sutar <112754652+Imran2909@users.noreply.github.com>
2025-02-04 01:11:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ const app = express();
 // Middleware for parsing requests
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({
-  origin: process.env.ORIGIN, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Change this to your frontend's URL when deployed
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true, // If using cookies/auth tokens
+  })
+);
 
 // Home route
 app.get("/", (req, res) => {
